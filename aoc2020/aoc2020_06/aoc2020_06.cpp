fdbe342b44aac5fbfc77493936b03e9648ed4e2f
Marius Bancila <marius.bancila@visma.com>
2020-12-08 00:27:04 +0200

    solved day 6 puzzle

--- aoc2020/aoc2020_06/aoc2020_06.cpp
+++ aoc2020/aoc2020_06/aoc2020_06.cpp
@@ -6,9 +6,90 @@
 #include <string>
 #include <string_view>
 #include <vector>
-#include <assert.h>
+#include <algorithm>
+#include <numeric>
+
+int count_group(std::string group)
+{
+   std::sort(group.begin(), group.end());
+   group.erase(std::unique(group.begin(), group.end()), group.end());
+   return group.size();
+}
+
+int get_groups_sum1(std::string_view filename)
+{
+   std::ifstream input(filename.data());
+   if (!input.is_open())
+      return 0;
+
+   int sum = 0;
+
+   std::string line;
+   std::string group;
+   while (std::getline(input, line))
+   {
+      if (!line.empty())
+      {
+         group += line;
+         continue;
+      }
+
+      sum += count_group(group);
+
+      group.clear();
+   }
+
+   if (!group.empty())
+   {
+      sum += count_group(group);
+   }
+
+   return sum;
+}
+
+int get_groups_sum2(std::string_view filename)
+{
+   std::ifstream input(filename.data());
+   if (!input.is_open())
+      return 0;
+
+   int sum = 0;
+   std::string line;
+   std::vector<int> counts(26);
+   int members = 0;
+
+   auto l_sum = [&counts](int const m) {
+      return std::accumulate(
+         counts.begin(), counts.end(),
+         0,
+         [m](int const total, int const n) {return total + ((n == m) ? 1 : 0); });
+   };
+
+   while (std::getline(input, line))
+   {
+      if (line.empty())
+      {
+         sum += l_sum(members);
+         std::fill(counts.begin(), counts.end(), 0);
+         members = 0;
+         continue;
+      }
+
+      for (auto const c : line)
+         counts[c - 'a']++;
+      members++;
+   }
+
+   sum += l_sum(members);
+
+   return sum;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   std::cout << get_groups_sum1(R"(..\data\aoc2020_06_input1_test.txt)") << '\n';
+   std::cout << get_groups_sum1(R"(..\data\aoc2020_06_input1.txt)") << '\n';
+
+   std::cout << get_groups_sum2(R"(..\data\aoc2020_06_input1_test.txt)") << '\n';
+   std::cout << get_groups_sum2(R"(..\data\aoc2020_06_input1.txt)") << '\n';
 }
