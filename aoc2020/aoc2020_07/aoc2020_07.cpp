6699a32fd59a47cd05ff942d8bb4204238bdfcb7
Marius Bancila <marius.bancila@visma.com>
2020-12-08 00:28:20 +0200

    added project for day 7 puzzle

---
+++ aoc2020/aoc2020_07/aoc2020_07.cpp
@@ -0,0 +1,12 @@
+// aoc2020_07.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string_view>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
