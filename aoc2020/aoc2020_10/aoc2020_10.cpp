b9528d730e34af7051202922a96f7185bab3099d
Marius Bancila <marius.bancila@visma.com>
2020-12-10 23:03:44 +0200

    solved puzzle 1 day 10

--- aoc2020/aoc2020_10/aoc2020_10.cpp
+++ aoc2020/aoc2020_10/aoc2020_10.cpp
@@ -6,8 +6,63 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <algorithm>
+
+std::vector<int> read_numbers(std::string_view filename)
+{
+   std::vector<int> numbers;
+
+   std::ifstream input(filename.data());
+   if (input.is_open())
+   {
+      std::string line;
+      while (std::getline(input, line))
+      {
+         numbers.push_back(std::stoi(line));
+      }
+   }
+
+   return numbers;
+}
+
+int find_joltage_factor(std::vector<int> data)
+{
+   int joltage[3]{0,0,0};
+
+   std::sort(data.begin(), data.end());
+
+   int jolts = 0;
+   for (auto const v : data)
+   {
+      joltage[v - jolts - 1]++;
+      jolts = v;
+   }
+
+   return joltage[0] * (joltage[2] + 1);
+}
+
+int find_distinct_ways(std::vector<int> data)
+{
+   return 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   std::vector<std::string> data
+   {
+      R"(..\data\aoc2020_10_input1_test.txt)",
+      R"(..\data\aoc2020_10_input2_test.txt)",
+      R"(..\data\aoc2020_10_input1.txt)",
+   };
+
+   for (auto const& filename : data)
+   {
+      auto numbers = read_numbers(filename);
+
+      auto factor = find_joltage_factor(numbers);
+      std::cout << factor << '\n';
+
+      auto count = find_distinct_ways(numbers);
+      std::cout << count << '\n';
+   }
 }
